41b651050c964c83cc08621b1f8cd1cacf2d3def
Luan Pessoa <luan.pessoa.393@hotmail.com>
2023-09-12 18:37:09 -0300

    ♻️ refactor: remove unusual components and views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<h1>Hello From HomeView</h1>
+	</main>
 </template>
+
+<script setup lang="ts"></script>
